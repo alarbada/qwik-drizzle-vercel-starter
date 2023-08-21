58f92ac44f60d661806e664b85d0d93fefd823cc
Guillem <you@example.com>
2023-08-21 17:38:05 +0200

    Add daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+const daisy = require("daisyui")
+const lightTheme = require('daisyui/src/theming/themes')['[data-theme=light]']
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+
+	daisyui: {
+		themes: [
+			{
+				light: {
+					...lightTheme,
+					// Overwrite all border radius
+					'--rounded-box': '0.2rem',
+					'--rounded-btn': '0.2rem',
+					'--rounded-badge': '0.2rem',
+				},
+			},
+		],
+	},
+
+	plugins: [daisy],
 }
